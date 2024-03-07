1de969778b622cb4ba4366d24b40ecb2edcf278d
Yael <yaelmexmontero@gmail.com>
2024-03-06 20:39:24 -0600

    Esqueleto Creado. Hagan sus ramas y a trabajar.

--- src/Main_AdminTareas.java
+++ src/Main_AdminTareas.java
@@ -3,11 +3,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JList;
 
 public class Main_AdminTareas extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textAsunto;
 
 	/**
 	 * Launch the application.
@@ -18,6 +27,7 @@ public class Main_AdminTareas extends JFrame {
 				try {
 					Main_AdminTareas frame = new Main_AdminTareas();
 					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -30,11 +40,87 @@ public class Main_AdminTareas extends JFrame {
 	 */
 	public Main_AdminTareas() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 859, 616);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("[Aquí irá el titúlo de la app junto con su decoración tipo banner]");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(0, 0, 845, 63);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Agregar Pendiente Nuevo");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_1.setBounds(88, 87, 224, 26);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Asunto:");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1_1.setBounds(22, 141, 87, 26);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Categoría:");
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1_1_1.setBounds(22, 192, 87, 26);
+		contentPane.add(lblNewLabel_1_1_1);
+		
+		JLabel lblNewLabel_1_1_1_1 = new JLabel("Importancia:");
+		lblNewLabel_1_1_1_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1_1_1_1.setBounds(22, 242, 101, 26);
+		contentPane.add(lblNewLabel_1_1_1_1);
+		
+		JLabel lblNewLabel_1_1_1_1_1 = new JLabel("Descripción:");
+		lblNewLabel_1_1_1_1_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel_1_1_1_1_1.setBounds(22, 296, 101, 26);
+		contentPane.add(lblNewLabel_1_1_1_1_1);
+		
+		textAsunto = new JTextField();
+		textAsunto.setBounds(129, 146, 230, 20);
+		contentPane.add(textAsunto);
+		textAsunto.setColumns(10);
+		
+		JComboBox comboCategoria = new JComboBox();
+		comboCategoria.setBounds(129, 196, 230, 22);
+		contentPane.add(comboCategoria);
+		
+		JRadioButton rdbtnRegular = new JRadioButton("Regular");
+		rdbtnRegular.setBounds(135, 246, 111, 23);
+		contentPane.add(rdbtnRegular);
+		
+		JRadioButton rdbtnImportante = new JRadioButton("Muy Importante");
+		rdbtnImportante.setBounds(263, 246, 122, 23);
+		contentPane.add(rdbtnImportante);
+		
+		JTextArea textDescripcion = new JTextArea();
+		textDescripcion.setLineWrap(true);
+		textDescripcion.setBounds(135, 300, 224, 93);
+		contentPane.add(textDescripcion);
+		
+		JButton btnAgregar = new JButton("Agregar Pendiente");
+		btnAgregar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnAgregar.setBounds(129, 443, 179, 43);
+		contentPane.add(btnAgregar);
+		
+		JList listPendientes = new JList();
+		listPendientes.setBounds(434, 141, 368, 252);
+		contentPane.add(listPendientes);
+		
+		JButton btnFiltar = new JButton("Filtrar Pendientes");
+		btnFiltar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnFiltar.setBounds(434, 414, 179, 43);
+		contentPane.add(btnFiltar);
+		
+		JButton btnMostrar = new JButton("Mostrar Pendiente");
+		btnMostrar.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnMostrar.setBounds(623, 414, 179, 43);
+		contentPane.add(btnMostrar);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Pendientes");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblNewLabel_1_2.setBounds(563, 87, 111, 26);
+		contentPane.add(lblNewLabel_1_2);
 	}
-
 }
--- src/Tareas.java
+++ src/Tareas.java
@@ -1,4 +1,41 @@
 
 public class Tareas {
+	private String asunto;
+	private String categoria;
+	private String importancia;
+	private String descripcion;
+	
+	public Tareas(String asunto, String categoria, String importancia, String descripcion) {
+		this.asunto = asunto;
+		this.categoria = categoria;
+		this.importancia = importancia;
+		this.descripcion = descripcion;
+	}
+	
+	public String getAsunto() {
+		return asunto;
+	}
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getImportancia() {
+		return importancia;
+	}
+	public void setImportancia(String importancia) {
+		this.importancia = importancia;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
 
 }
