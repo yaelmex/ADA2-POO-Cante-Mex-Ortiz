8b0a7b21b66669809a29e089e3e885f8b9c73706
viane <viane.cante@hotmail.com>
2024-03-08 20:02:35 -0600

    Vianey agregó el método buscar.
    Dicho método servirá para buscar alguna tarea a la pila considerando si
    su asunto es regular o muy importante, para ello utilizamos un for el
    cual nos permitirá recorrer las tareas

--- src/Tareas.java
+++ src/Tareas.java
@@ -11,6 +11,17 @@ public class Tareas {
 		this.importancia = importancia;
 		this.descripcion = descripcion;
 	}
+
+	//Método buscar realizado por Vianey
+	/*Con este método podemos buscar dentro de la pila de tareas
+	  tomando en cuenta si el asunto es regular o muy importante*/
+	public Tareas buscar(String buscar) {
+		for(Tareas asunto : tarea) {//el bucle nos permite recorrer cada tarea de la pila
+			if(asunto.getAsunto()) {//verifica que el asunto de la tarea en la que se encuentra sea verdadero o no
+				return asunto;//devuelve la tarea
+			}
+		}
+	}
 	
 	public String getAsunto() {
 		return asunto;
