8b2c2de293c3f16a67482a7cc8b48ae40fc1b66f
viane <viane.cante@hotmail.com>
2024-03-09 18:03:55 -0600

    Decoración del fondo del frame_Vianey

--- src/Main_AdminTareas.java
+++ src/Main_AdminTareas.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -52,6 +53,7 @@ public class Main_AdminTareas extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 859, 616);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(128, 128, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
